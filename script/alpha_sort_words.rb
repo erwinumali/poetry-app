d70fbff649b32925e5e24d5af53bdd0b402ad745
Aaron <mail@aapomm.io>
2024-12-24 22:39:47 +0800

    Add more words

--- script/alpha_sort_words.rb
+++ script/alpha_sort_words.rb
@@ -2,9 +2,9 @@ require 'yaml'
 
 easy_words = YAML.load_file('db/easy_words.yml')
 
-easy_words = easy_words['easy'].uniq.map(&:downcase).sort
+easy_words = easy_words['easy'].map(&:downcase).sort.uniq
 File.open('db/new_easy_words.yml', 'w') { |f| f.write({ 'easy' => easy_words }.to_yaml) }
 
 hard_words = YAML.load_file('db/hard_words.yml')
-hard_words = hard_words['hard'].uniq.map(&:downcase).sort
+hard_words = hard_words['hard'].map(&:downcase).sort.uniq
 File.open('db/new_hard_words.yml', 'w') { |f| f.write({ 'hard' => hard_words }.to_yaml) }
